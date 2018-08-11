138dcf96f10e5ff47e906a00c0193f1718829d32
ttthanh <trantrungthanh910@gmail.com>
2018-08-11 08:24:09 +0700

    CC

--- js/map.js
+++ js/map.js
@@ -17,3 +17,11 @@ function RedirectService(){
 function RedirectAbout(){
 	window.location.replace("../about.html");
 }
+
+function myMap() {
+	var mapProp= {
+	    center:new google.maps.LatLng(51.508742,-0.120850),
+	    zoom:5,
+	};
+	var map=new google.maps.Map(document.getElementById("googleMap"),mapProp);
+}
